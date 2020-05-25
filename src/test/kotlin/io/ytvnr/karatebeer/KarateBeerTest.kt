0b7f0860b107f0ffa4756a2ecea83316f22bf88e
Yann Tavernier <yann.tavernier@qima.com>
2020-05-25 08:12:28 +0200

    test: add parallel testing (with errors)

--- src/test/kotlin/io/ytvnr/karatebeer/KarateBeerTest.kt
+++ src/test/kotlin/io/ytvnr/karatebeer/KarateBeerTest.kt
@@ -1,6 +1,9 @@
 package io.ytvnr.karatebeer
 
+import com.intuit.karate.Runner
 import com.intuit.karate.junit5.Karate
+import org.junit.Assert.assertTrue
+import org.junit.jupiter.api.Test
 
 class KarateBeerTest {
 
@@ -8,4 +11,10 @@ class KarateBeerTest {
     fun testFullPath(): Karate? {
         return Karate.run().tags("~@ignore").relativeTo(javaClass)
     }
+
+//    @Test
+//    fun testParallel() {
+//        val results = Runner.path("classpath:io/ytvnr/karatebeer").tags("~@ignore").parallel(5)
+////        assertTrue(results.errorMessages, results.failCount == 0)
+//    }
 }
